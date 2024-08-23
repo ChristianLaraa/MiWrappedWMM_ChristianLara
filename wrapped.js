3e6a55fb4dcc01c52134de211ccada5eef1cb540
Christian Lara <christian.lara.091204@gmail.com>
2024-08-23 00:33:37 -0600

    add sections and code w js

--- wrapped.js
+++ wrapped.js
@@ -123,3 +123,81 @@ const percentageDep = new Chart(percentageDepor, {
         }
     }
 });
+const percentageEnt = document.getElementById('percentageEntre').getContext('2d');
+const percentageEntre = new Chart(percentageEnt, {
+    type: 'pie',
+    data: {
+        labels: ['Spotify', 'Youtube', 'Tik Tok', 'Twitch', 'Netflix'],
+        datasets: [{
+            label: 'Porcentajes',
+            data: [45, 23, 15, 10, 7 ], // Estos números deben sumar 100
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: 'top',
+            },
+            tooltip: {
+                callbacks: {
+                    label: function(context) {
+                        let label = context.label || '';
+                        if (label) {
+                            label += ': ';
+                        }
+                        label += context.raw + '%';
+                        return label;
+                    }
+                }
+            }
+        }
+    }
+});
+
+const searchInput = document.getElementById('search');
+const eventsList = document.getElementById('events-list');
+
+// Filtrado de eventos
+searchInput.addEventListener('input', function() {
+    const query = searchInput.value.toLowerCase();
+    const events = eventsList.getElementsByTagName('li');
+
+    for (let event of events) {
+        const title = event.querySelector('h3').innerText.toLowerCase();
+        if (title.includes(query)) {
+            event.style.display = '';
+        } else {
+            event.style.display = 'none';
+        }
+    }
+});
+
+// Mostrar más detalles
+const toggleButtons = document.querySelectorAll('.toggle-details');
+
+toggleButtons.forEach(button => {
+    button.addEventListener('click', function() {
+        const moreDetails = this.nextElementSibling; // Obtener el siguiente elemento (detalles adicionales)
+        if (moreDetails.style.display === "none" || moreDetails.style.display === "") {
+            moreDetails.style.display = "block"; // Mostrar detalles
+            this.innerText = "Mostrar menos"; // Cambiar texto del botón
+        } else {
+            moreDetails.style.display = "none"; // Ocultar detalles
+            this.innerText = "Mostrar más"; // Cambiar texto del botón
+        }
+    });
+});
+
