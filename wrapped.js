a39daeecf50fa3c7eb33ac824c0f4b311988d8d3
Christian Lara <christian.lara.091204@gmail.com>
2024-08-23 22:37:51 -0600

    add ult version no responsive

--- wrapped.js
+++ wrapped.js
@@ -217,19 +217,7 @@ const searchInput = document.getElementById('search');
 const eventsList = document.getElementById('events-list');
 
 // Filtrado de eventos
-searchInput.addEventListener('input', function() {
-    const query = searchInput.value.toLowerCase();
-    const events = eventsList.getElementsByTagName('li');
 
-    for (let event of events) {
-        const title = event.querySelector('h3').innerText.toLowerCase();
-        if (title.includes(query)) {
-            event.style.display = '';
-        } else {
-            event.style.display = 'none';
-        }
-    }
-});
 
 // Mostrar más detalles
 const toggleButtons = document.querySelectorAll('.toggle-details');
@@ -246,4 +234,142 @@ toggleButtons.forEach(button => {
         }
     });
 });
+// Bar Chart
+const barCtx = document.getElementById('barChart').getContext('2d');
+const barChart = new Chart(barCtx, {
+    type: 'bar',
+    data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August'],
+        datasets: [{
+            label: 'Cruz Cafune',
+            data: [1, 1, 20, 1, 1, 1, 1, 1],
+            backgroundColor: 'rgba(255, 99, 132, 0.2)',
+            borderColor: 'rgba(255, 99, 132, 1)',
+            borderWidth: 1
+        }, {
+            label: 'AquihayAquihay',
+            data: [1, 1, 1, 1, 20, 1, 1, 1],
+            backgroundColor: 'rgba(54, 162, 235, 0.2)',
+            borderColor: 'rgba(54, 162, 235, 1)',
+            borderWidth: 1
+        }, {
+            label: 'Tino el Pingüino',
+            data: [1, 1, 1, 1, 1, 1, 1, 20],
+            backgroundColor: 'rgba(75, 192, 192, 0.2)',
+            borderColor: 'rgba(75, 192, 192, 1)',
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
 
+// Pie Chart
+const pieCtx = document.getElementById('pieChart').getContext('2d');
+const pieChart = new Chart(pieCtx, {
+    type: 'pie',
+    data: {
+        label: 'Horas',
+        labels: ['Gym', 'Futbol'],
+        datasets: [{
+            data: [24, 6],
+            backgroundColor: ['rgba(255, 159, 64, 0.2)', 'rgba(153, 102, 255, 0.2)'],
+            borderColor: ['rgba(255, 159, 64, 1)', 'rgba(153, 102, 255, 1)'],
+            borderWidth: 1
+        }]
+    }
+});
+
+// Doughnut Chart
+const doughnutCtx = document.getElementById('doughnutChart').getContext('2d');
+const doughnutChart = new Chart(doughnutCtx, {
+    type: 'doughnut',
+    data: {
+        labels: ['Spotify', 'Youtube', 'Tik Tok', 'Twitch', 'Netflix'],
+        datasets: [{
+            data: [95, 40, 27, 12.5, 9],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)'
+            ],
+            borderWidth: 1
+        }]
+    }
+});
+
+const barrCtx = document.getElementById('barrChart').getContext('2d');
+const barrChart = new Chart(barrCtx, {
+    type: 'bar',
+    data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August'],
+        datasets: [{
+            label: 'Semana Diabla - HoV',
+            data: [1, 1, 1, 3, 1, 1, 1, 1],
+            backgroundColor: 'rgba(255, 99, 132, 0.2)',
+            borderColor: 'rgba(255, 99, 132, 1)',
+            borderWidth: 1
+        }, {
+            label: 'Hackathon, AngelHack',
+            data: [1, 1, 1, 18, 1, 1, 1, 1],
+            backgroundColor: 'rgba(54, 162, 235, 0.2)',
+            borderColor: 'rgba(54, 162, 235, 1)',
+            borderWidth: 1
+        }, {
+            label: 'Gamergy Mexico',
+            data: [1, 1, 1, 1, 1, 1, 1, 8],
+            backgroundColor: 'rgba(75, 192, 192, 0.2)',
+            borderColor: 'rgba(75, 192, 192, 1)',
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+
+// Second Pie Chart
+const secondPieCtx = document.getElementById('secondPieChart').getContext('2d');
+const secondPieChart = new Chart(secondPieCtx, {
+    type: 'pie',
+    data: {
+        labels: ['GTA', 'Valorant', 'Rocket', 'Fall Guys', 'Mortal Kombat'],
+        datasets: [{
+            label: 'Horas',
+            data: [9, 8, 8, 5, 5],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)'
+            ],
+            borderWidth: 1
+        }]
+    }
+});
